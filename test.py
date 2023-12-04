5e3dbb70f3de5335dafb8fbcfe193938e1ed40b9
k200481 <k200481@nu.edu.pk>
2023-12-04 18:37:20 +0500

    cleanup

--- test.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-conn = mysql.connector.connect(
-    user="sarim",password="12345678",database='db_database'
-)
-
-cur = conn.cursor(dictionary=True)
-cur.execute('select * from author where id = 5')
-print(cur.fetchone())
